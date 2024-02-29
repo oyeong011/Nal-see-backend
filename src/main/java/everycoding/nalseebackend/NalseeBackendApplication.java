5ff1c3e76b73ae9c23eb117185d113efdf792398
Seungmok1 <kww4215@naver.com>
2024-02-29 22:14:23 +0900

    first commit

---
+++ src/main/java/everycoding/nalseebackend/NalseeBackendApplication.java
@@ -0,0 +1,13 @@
+package everycoding.nalseebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NalseeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NalseeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/everycoding/nalseebackend/NalseeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package everycoding.nalseebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NalseeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
