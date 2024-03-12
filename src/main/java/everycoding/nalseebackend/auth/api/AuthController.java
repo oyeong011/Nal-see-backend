d6cdd16b307c086249df6028f5f3ec9fc4830f9a
Hyejin724 <148074385+Hyejin724@users.noreply.github.com>
2024-03-12 18:43:02 +0900

    [edit] NS-10 oauth2 2차 작성안

---
+++ src/main/java/everycoding/nalseebackend/auth/api/AuthController.java
@@ -0,0 +1,10 @@
+package everycoding.nalseebackend.auth.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+}
