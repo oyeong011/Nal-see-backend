517a9f729607b19c3c673f7c94f2410d7716a08f
Seungmok1 <kww4215@naver.com>
2024-03-20 17:59:54 +0900

    feat: 포스트 검색

--- src/main/java/everycoding/nalseebackend/post/PostRepository.java
+++ src/main/java/everycoding/nalseebackend/post/PostRepository.java
@@ -4,13 +4,14 @@ import everycoding.nalseebackend.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
     Page<Post> findByIdLessThan(Long lastPostId, Pageable pageable);
 
